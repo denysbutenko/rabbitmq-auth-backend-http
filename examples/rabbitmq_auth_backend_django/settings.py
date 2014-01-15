4b6a2278b07028c33de19b1e2a9b70f026c9e6c3
Denys Butenko <ua13dark@gmail.com>
2014-01-15 21:12:45 +0200

    Update for Django 1.6

--- examples/rabbitmq_auth_backend_django/settings.py
+++ examples/rabbitmq_auth_backend_django/settings.py
@@ -3,19 +3,24 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+import os
+
+
+def rel(*x):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/tmp/rabbitmq_auth_backend_django.sqlite'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': rel('..', '..', 'db', 'rabbitmq.db'),
+    }
+}
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -51,22 +56,31 @@ ADMIN_MEDIA_PREFIX = '/media/'
 SECRET_KEY = 'np2&hws)m)(hv_ne=%+y5syjd$#l^ab#rz%g)zq)^e5tyro&xz'
 
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
-)
+if DEBUG:
+    TEMPLATE_LOADERS = [
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    ]
+else:
+    TEMPLATE_LOADERS = [
+        ('django.template.loaders.cached.Loader', (
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        )),
+    ]
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'rabbitmq_auth_backend_django.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    'templates',
+    # Put strings here, like "/home/html/django_templates"
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
--- examples/rabbitmq_auth_backend_django/urls.py
+++ examples/rabbitmq_auth_backend_django/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Example:
-    (r'^auth/user',     'auth.views.user'),
-    (r'^auth/vhost',    'auth.views.vhost'),
+urlpatterns = patterns(
+    '',
+    (r'^auth/user', 'auth.views.user'),
+    (r'^auth/vhost', 'auth.views.vhost'),
     (r'^auth/resource', 'auth.views.resource'),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
